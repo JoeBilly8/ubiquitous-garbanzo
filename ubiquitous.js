18716e4250818fda0770286a7f4beba5a29f07fc
Joe Moore <joebilly8@hotmail.co.uk>
2025-05-14 12:46:13 +1000

    type coercion

--- ubiquitous.js
+++ ubiquitous.js
@@ -39,7 +39,44 @@ console.log(blockScoped) // -> ReferenceError: blockScoped is not defined
 
 // Whereas if you do the above with var, it will be available outside of the block (because it's function scoped)
 // var is hoisted to the top of the function, so it will be available outside of the block
+// if its not inside a function, it will be available globally
 if (true) {
     var functionScoped = 3;
 }
 console.log(functionScoped) // -> 3
+
+// Hoisting example
+// Hoisting is when a variable is declared at the top of the function, regardless of where it is defined
+// This means that you can use the variable before it is defined, but it will be undefined until it is defined
+function test() {
+    console.log(x) // -> undefined
+    var x = 5
+    console.log(x) // -> 5
+}
+// Whereas with let and const, they are not hoisted to the top of the function, so you will get a ReferenceError if you try to access them before they are defined
+
+//// Arithmetic operators
+// + - * / % ** ++ --
+// + is addition
+// - is subtraction
+// * is multiplication
+// / is division
+// % is modulus (remainder)
+// ** is exponentiation
+// ++ is increment
+// -- is decrement
+
+
+// With arithmetic operators, we can expect some weird behavior
+// EG:
+const z = 1
+const y = '1'  
+console.log(x + y) // -> 11 (string concatenation)
+// This is because the + operator is overloaded, so it will try to convert the number to a string and then concatenate it
+// This is called type coercion
+// Type coercion is when JavaScript tries to convert a value to a different type
+// EG:
+const a = 1
+const b = '1'
+console.log(a == b) // -> true (loose equality)
+// This is because == will try to convert the values to the same type before comparing them
